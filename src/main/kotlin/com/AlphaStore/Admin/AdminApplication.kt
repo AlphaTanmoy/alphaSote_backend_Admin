0ff94241810c434b48ee340cee492431f9cd1542
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 10:33:41 +0530

    main file uploaded

--- src/main/kotlin/com/AlphaStore/Admin/AdminApplication.kt
+++ src/main/kotlin/com/AlphaStore/Admin/AdminApplication.kt
@@ -2,8 +2,10 @@ package com.alphaStore.Admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class AdminApplication
 
 fun main(args: Array<String>) {
